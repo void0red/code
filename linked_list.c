e6e64b5c5584864ffe4330ae763799e6bbaa5bae
void0red <769713482@qq.com>
2017-10-24 12:14:53 +0800

    add

--- linked_list.c
+++ linked_list.c
@@ -13,6 +13,7 @@ node * node_init();
 node * node_add(node *head, int data);
 node * node_insert(node *head, int i,int data);
 void node_del(node *head, int i);
+node * node_modify(node *head, int i, int data);
 
 void list_print(node *head);
 int list_length(node *head);
@@ -25,10 +26,10 @@ int main(){
 	node * head = node_init();
 	node_add(head, 1);
 	node_add(head, 2333);
-	node_insert(head, 1, 200);
-	node_insert(head, list_length(head) -1, 250);
-	node_del(head, 5);
-
+	// node_insert(head, 1, 200);
+	// node_insert(head, list_length(head) -1, 250);
+	// node_del(head, 5);
+	node_modify(head,4,233);
 	list_print(head);
 	printf("%d\n",list_length(head));
 
@@ -109,4 +110,16 @@ void list_del(node *head){
 		p1 = p1 -> next;
 		free(p2);
 	}
+}
+
+node * node_modify(node *head, int i, int data){
+	node *p = head;
+	while(i > 0){
+		if(p -> next == NULL)return NULL;
+		p = p -> next;
+		i--;
+	}
+	p -> data = data;
+	return p;
+
 }
